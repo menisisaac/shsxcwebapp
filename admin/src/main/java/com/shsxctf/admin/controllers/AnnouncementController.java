822448dfe52102b7fd3a4faa0e3e162d632bb137
Isaac Menis <imenis@albany.edu>
2022-07-21 12:36:21 -0400

    changed library

--- admin/src/main/java/com/shsxctf/admin/controllers/AnnouncementController.java
+++ admin/src/main/java/com/shsxctf/admin/controllers/AnnouncementController.java
@@ -4,9 +4,12 @@ import com.shsxctf.admin.mappers.AnnoucementMapper;
 import com.shsxctf.admin.models.Annoucement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @RestController
@@ -15,6 +18,12 @@ public class AnnouncementController {
     @Autowired
     JdbcTemplate template;
 
+    @PostMapping("/annoucement")
+    @CrossOrigin
+    public void addAnnoucement() {
+        String sql = "INSERT INTO announcement(date, description) VALUES (?, ?)";
+    }
+
     @GetMapping("/announcement")
     @CrossOrigin
     public List<Annoucement> getAnnouncements() {
@@ -22,16 +31,7 @@ public class AnnouncementController {
         return template.query(sql, new AnnoucementMapper());
     }
 
-    @PostMapping("/annoucement")
-    @CrossOrigin
-    public void addAnnoucement() {
-        String sql = "INSERT INTO announcement(date, description) VALUES (?, ?)";
-    }
 
-    public void editAnnoucement() {
-    }
 
-    public void deleteMapping() {
-    }
 
 }
