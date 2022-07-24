f5166df3657ab229f6fc8cf87b4d7afd98830b14
Isaac Menis <imenis@albany.edu>
2022-07-24 14:29:53 -0400

    add team info

--- shsxctf/src/App.js
+++ shsxctf/src/App.js
@@ -9,6 +9,7 @@ import Outdoortf from "./Outdoortf";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Grout from "./Grout";
 import TestNavBar from "./TestNavBar";
+import TeamInfo from "./TeamInfo";
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
           <Route exact path='/indoortf'><Indoortf/></Route>
           <Route exact path='/outdoortf'><Outdoortf/></Route>
           <Route exact path="/admin"><Dashboard/></Route>
+          <Route exact path="/info"><TeamInfo/></Route>
           <Route exact path="/"><Home/></Route>
         </Switch>
       </div>
---
+++ shsxctf/src/TeamInfo.js
@@ -0,0 +1,50 @@
+const TeamInfo = () => {
+    return (
+        <div>
+            <div className="gear">
+            <div class="accordion" id="accordionExample">
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Gear/Clothing
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Gear is simple for cross country.  You need shorts,t-shirt, and running shoes.  Running shoes are most important.  The shoe you get depends on  what feels comfortable for you.  It must be classified as a running shoe.  You can go to any sporting goods store to find running shoes or fleet feet.  They do not have to be expensive  but they need to be running shoes.  I also have a few shoes available for use as needed.
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingTwo">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        Summer Training
+      </button>
+    </h2>
+    <div id="collapseTwo" class="accordion-collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Summer training is important for cross country to make you the best possible runner. There  is a summer training program on google classroom at this LINK.  Bottom line is you need to get out there and run for 20 minutes to 40 minutes.  Also getting to a gym and working out, doing outdoor activities, doing core, doing anything that is not just being on electronic devices helps
+      </div>
+    </div>
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingThree">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        Practice Info
+      </button>
+    </h2>
+    <div id="collapseThree" class="accordion-collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Typical practice for summer and school year can be similar.  You should do a warmup of  easy running,  drills, and a workout for day which could range from an easy run to a fast track workout. Practice always ends with a core workout and basic stretching. <a href="https://www.dartmouth-hitchcock.org/sites/default/files/2020-12/dynamic-stretching-for-athletes.pdf">Stretches</a> <a href="http://www.rsgplus.org/wp-content/uploads/2017/10/top_core_exercises.pdf">Core</a> <a href="https://jasonvaladao.com/wp-content/uploads/2020/05/MYRTLE_ROUTINE.pdf">Strength</a>
+      </div>
+    </div>
+  </div>
+</div>
+            </div>
+            <div className="summer"></div>
+            <div classname="extra"></div>
+        </div>
+    );
+}
+ 
+export default TeamInfo;
